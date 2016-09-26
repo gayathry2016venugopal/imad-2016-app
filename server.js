ede0b71037ef29956c736e399e29036ddf550c79
gayathry2016venugopal <gayathry2016venugopal@gmail.com>
2016-09-26 13:00:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+}());
+((())'app.app.get('/article-two'function (req,res)get('/ui/style.css', function (req, res) {
+)  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.app.get('/article-three function (req,res)get('/ui/style.css', function (req, res)});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
